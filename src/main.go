9bc0a00c0f781c326e32ccf79ef525e2e152da3a
RockJew <827872097@qq.com>
2024-01-03 16:27:16 +0800

    add

--- src/main.go
+++ src/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "github.com/go-resty/resty/v2"
+import (
+	"fmt"
+	"github.com/go-resty/resty/v2"
+)
 
 func httpPost() {
 	appToken := "AT_r9L2ORNpkVOy9HdoQ5YHra8ag10oDjX8"
@@ -9,19 +12,22 @@ func httpPost() {
 	targetUrl := "https://wxpusher.zjiecode.com/api/send/message"
 
 	client := resty.New()
-	body := map[string]interface{} {
-		"appToken": appToken,
-		"content": content,
-		"summary": content,
+	body := map[string]interface{}{
+		"appToken":    appToken,
+		"content":     content,
+		"summary":     content,
 		"contentType": 1,
-		"topicIds": []int{68682},
+		"topicIds":    []int{25109},
 	}
 
-	resp, err := client.R().
-		SetHeader("Content-Type"j, "application/json").
+	resp, _ := client.R().
+		SetHeader("Content-Type", "application/json").
+		SetBody(body).
+		Post(targetUrl)
 
+	fmt.Print(resp)
 }
 
 func main() {
-
+	httpPost()
 }
