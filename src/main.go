10c0b06caf1eae9f988f2eb0e7b0bdb7fbfa763b
RockJew <827872097@qq.com>
2024-02-17 17:43:48 +0800

    add

--- src/main.go
+++ src/main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/go-resty/resty/v2"
 )
 
-func httpPost() {
-	appToken := "AT_r9L2ORNpkVOy9HdoQ5YHra8ag10oDjX8"
-	content := "测试"
+const STOCK_GZ string = "sh000012"
+const STOCK_300 string = "sh000300"
+const STOCK_NSDK string = "sz159632"
+const STOCK_CYCLE int = 22
 
-	targetUrl := "https://wxpusher.zjiecode.com/api/send/message"
+const NOTICE_APPTOKEN string = "AT_r9L2ORNpkVOy9HdoQ5YHra8ag10oDjX8"
+
+// 发送通知
+func pushNotification(content, summary string) {
+	var targetUrl = "https://wxpusher.zjiecode.com/api/send/message"
 
 	client := resty.New()
 	body := map[string]interface{}{
-		"appToken":    appToken,
+		"appToken":    NOTICE_APPTOKEN,
 		"content":     content,
-		"summary":     content,
+		"summary":     summary,
 		"contentType": 1,
 		"topicIds":    []int{25109},
 	}
@@ -28,6 +34,32 @@ func httpPost() {
 	fmt.Print(resp)
 }
 
+// 获取股票当前值
+func getCurrentValue(stockCode string) float64 {
+	var url = "https://hq.sinajs.cn/list=%s"
+	var targetUrl = fmt.Sprintf(url, stockCode)
+
+	client := resty.New()
+	resp, _ := client.R().
+		SetHeader("Referer", "https://finance.sina.com.cn").
+		Get(targetUrl)
+
+	fmt.Print(resp)
+	return 1.0
+}
+
+// 获取股票历史数据
+func getHistoryValue(stockCode string, daysBefore int) float64 {
+	var url = "https://quotes.sina.cn/cn/api/jsonp_v2.php/data/CN_MarketDataService.getKLineData?symbol=%s&scale=240&ma=no&datalen=24"
+	var targetUrl = fmt.Sprintf(url, stockCode)
+
+	client := resty.New()
+	resp, _ := client.R().Get(targetUrl)
+
+	fmt.Print(resp)
+	return 1.0
+}
+
 func main() {
-	httpPost()
+	getCurrentValue(STOCK_GZ)
 }
